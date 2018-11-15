83da3cff23fcbeeaf06a16e0782a84b0cc3445fd
David Jonas <djonas@noip.com>
2018-11-15 12:26:26 -0800

    Fix up lookup_a()

--- src/main.rs
+++ src/main.rs
@@ -63,8 +63,36 @@ fn lookup_a(
     ns: SocketAddr,
     qname: Name,
 ) -> impl Future<Item = Ipv4Addr, Error = trust_dns::error::ClientError> {
-    let (stream, handle) = UdpClientStream::new(ns);
-    let (bg, mut client) = ClientFuture::new(stream, handle, None);
+    lazy(move || {
+        let (stream, handle) = UdpClientStream::new(ns);
+        let (bg, mut client) = ClientFuture::new(stream, handle, None);
+
+        tokio::spawn(bg);
+
+        client
+            .query(qname, DNSClass::IN, RecordType::A)
+            .and_then(|response| {
+                let answers = response.answers();
+
+                if answers.len() == 0 {
+                    return futures::future::err(
+                        trust_dns::error::ClientErrorKind::Msg("No answers".to_string()).into(),
+                    );
+                }
+
+                return match answers[0].rdata() {
+                    &RData::A(ref ip) => futures::future::ok(*ip),
+                    record => futures::future::err(
+                        trust_dns::error::ClientErrorKind::Msg(
+                            format!("Unexpected answer type; {}", record.to_record_type())
+                                .to_string(),
+                        )
+                        .into(),
+                    ),
+                };
+            })
+    })
+}
 
     tokio::spawn(bg);
 
