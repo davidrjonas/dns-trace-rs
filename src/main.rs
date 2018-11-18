35b47828a22ef2ebdc6ac31c4912ea5a84b43051
David Jonas <djonas@noip.com>
2018-11-17 22:00:59 -0800

    Adds cli arg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-extern crate chrono;
+#[macro_use]
+extern crate clap;
 extern crate futures;
 extern crate rand;
 extern crate tokio;
@@ -6,8 +7,8 @@ extern crate trust_dns;
 extern crate trust_dns_proto;
 
 use std::str::FromStr;
-//use std::time::Duration;
 
+use clap::{App, Arg};
 use tokio::runtime::current_thread::Runtime;
 use trust_dns::rr::Name;
 
@@ -15,13 +16,21 @@ mod dns_trace;
 use dns_trace::trace;
 
 fn main() {
+    let matches = App::new("dns-trace")
+        .about("Performs DNS queries starting at the root Internet name servers following the specified authorities until it finds an answer.")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .arg(Arg::with_name("HOST")
+             .help("The hostname to query")
+             .required(true)
+             .index(1))
+        .get_matches();
+
+    let qname = Name::from_str(matches.value_of("HOST").unwrap()).unwrap();
+
     let mut runtime = Runtime::new().unwrap();
 
-    let qname = Name::from_str("noip.co.uk").unwrap();
-
     let _trace = runtime
-        .block_on(trace(&qname, Some(|s| println!("Step: {}", s))))
+        .block_on(trace(&qname, Some(|s| println!("{}", s))))
         .unwrap();
-
-    //println!("Trace: {:?}", trace);
 }
